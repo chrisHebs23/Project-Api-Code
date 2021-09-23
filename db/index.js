d745fd7c1175c7933f320defd518969d31e69fba
chrisHebs23 <christopher.heberden@gmail.com>
2021-09-23 16:15:02 -0700

    fix proxy errors

--- db/index.js
+++ db/index.js
@@ -1,21 +1,23 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const devConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-};
+// const devConfig = {
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT,
+// };
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL,
-};
+const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_DATABASE}
+/${process.env.DB_PORT}`;
 
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+const proConfig = process.env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+});
 
 module.exports = {
   query: (text, params, callback) => {
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = require("./db");
 //create server
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 
@@ -26,7 +26,7 @@ app.use(
   })
 );
 
-var swaggerDefinition = {
+const swaggerDefinition = {
   info: {
     title: "Node Swagger API",
     version: "1.0.0",
@@ -55,6 +55,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(flash());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static());
+}
 
 app.get("/", (req, res) => {
   res.send("Hello");

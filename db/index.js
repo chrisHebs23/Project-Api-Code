cee3a36d08e1bb318a98f982e8417bfdf0c5f1b8
chrisHebs23 <christopher.heberden@gmail.com>
2021-09-24 14:33:29 -0700

    add client

--- db/index.js
+++ db/index.js
@@ -1,13 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
-
-// const devConfig = {
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DATABASE,
-//   port: process.env.DB_PORT,
-// };
+const { Client } = require("pg");
 
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_DATABASE}
 /${process.env.DB_PORT}`;
@@ -18,6 +11,26 @@ const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
+
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres:postgres@localhost:5432/projectapi",
+  ssl: process.env.DATABASE_URL ? true : false,
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(row);
+    }
+    client.end();
+  }
+);
 
 module.exports = {
   query: (text, params, callback) => {
